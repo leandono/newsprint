74c9565c6db44671314e3efd1a119f06d888bf19
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2015-05-24 14:53:15 -0300

    Debug configuration

--- bin/libs/logger/index.js
+++ bin/libs/logger/index.js
@@ -4,6 +4,7 @@
  * Main modules
  */
 var logger = require('winston');
+var program = require('commander');
 
 /**
  * Logger setup
@@ -17,10 +18,12 @@ logger.remove(logger.transports.Console);
 
 //Set logging using the console
 logger.add(logger.transports.Console, {
-	level: 'info',
+	level: process.env.DEBUG ? 'debug' : 'info',
 	silent: false,
 	colorize: true
 });
+
+program.parse(process.argv);
 
 /**
  * Public methods exported
